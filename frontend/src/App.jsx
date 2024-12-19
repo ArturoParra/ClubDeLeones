8e24f327e39e72caa3ee5f6754f2edf27067a8a7
Arturo <aparrar1900@alumno.ipn.mx>
2024-12-18 19:59:03 -0600

    Header, pagina principal y configuracion de rutas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,22 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
+import { Routes, Route } from 'react-router-dom';
+import { IndexPage } from "./views/IndexPage";
 
 function App() {
-    const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
 
-    useEffect(() => {
-        fetch('http://127.0.0.1:5000/api')
-            .then((response) => response.json())
-            .then((data) => setMessage(data.message))
-            .catch((error) => console.error('Error:', error));
-    }, []);
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/api")
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error("Error:", error));
+  }, []);
 
-    return (
-        <div>
-            <h1>{message}</h1>
-        </div>
-    );
+  return (
+    <Routes>
+      <Route exact path="/" element={<IndexPage />} />
+    </Routes>
+  );
 }
 
 export default App;
